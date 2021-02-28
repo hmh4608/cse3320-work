3310163bdb942b24bd10b60d55359fc27b4f8398
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2021-02-28 00:04:36 -0600

    Update msh.c

--- shell/msh.c
+++ shell/msh.c
@@ -76,7 +76,7 @@ int main()
     memset(&history[history_pos], 0, MAX_COMMAND_SIZE);
     if(working_str != NULL) //in case the user only presses enter
     {
-        strncpy(history[history_pos], working_str, MAX_COMMAND_SIZE);
+        strncpy(&(history[history_pos]), working_str, MAX_COMMAND_SIZE);
         history_pos++;
     }
 
@@ -163,7 +163,7 @@ int main()
 
     free( working_root );
   }
-  return 0;
+  exit(EXIT_SUCCESS);
 }
 
 /*
