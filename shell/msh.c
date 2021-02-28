8b043b8195419e6e095be151d8d128b7d3af8b5a
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2021-02-28 01:17:26 -0600

    Update msh.c

--- shell/msh.c
+++ shell/msh.c
@@ -79,9 +79,12 @@ int main()
         history_pos = 0;
     }
     //set all values in the string to NULL in case a shorter string overwrites it later
-    memset(&history[history_pos], 0, MAX_COMMAND_SIZE);
-    strcpy(history[history_pos], working_str);
-    history_pos++;
+    if(strcmp(working_str, "\n") != 0)
+    {
+        memset(&history[history_pos], 0, MAX_COMMAND_SIZE);
+        strcpy(history[history_pos], working_str);
+        history_pos++;
+    }
 
     // we are going to move the working_str pointer so
     // keep track of its original value so we can deallocate
