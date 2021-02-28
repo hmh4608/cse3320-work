4ae04ff6e2e1863d9538aba4cd0a0083bcd2a16c
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2021-02-27 19:59:50 -0600

    Update msh.c

--- shell/msh.c
+++ shell/msh.c
@@ -97,7 +97,7 @@ int main()
             printf("%d: %d\n", i, pids[i]);
         }
     }
-    else if(arguments[0] != NULL)
+    else if(!(strcmp(arguments[0], NULL) == 0))
     {
         pids[pids_index++] = fork();
         int status;
